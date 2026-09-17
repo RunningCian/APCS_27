6207f2451e935e3d7db8cbb997731ba2e42769be
Cian Byrne <cbyrne3125@gmail.com>
2026-09-17 20:32:07 +0000

    update

--- 102_Exercises/09_Restaurant/basecode/starter.java
+++ 102_Exercises/09_Restaurant/basecode/starter.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
+Scanner sc = new Scanner(System.in);
+		System.out.print("What is your name?");
+		String n = sc.nextLine();
+
 		
 	}
 }
